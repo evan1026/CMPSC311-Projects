ae92b4ddb7abbfcc85595053962d0fdc923ad935
Michael Berezanich <michaelberezanich@gmail.com>
2016-02-03 21:19:03 -0500

    Added input files and file opening

--- Project_1/src/main.c
+++ Project_1/src/main.c
@@ -1,1 +1,39 @@
-int main() {}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+    FILE *input_textfile = fopen(argv[1, "r");
+    FILE *output_countfile = fopen(argv[2], "w");
+    FILE *output_runtime = fopen(argv[3], "w");
+
+    if (check_files(FILE input_textfile, FILE output_countfile, FILE output_runtime){
+        printf("All files opened succesfully");
+    }
+
+    
+
+}
+
+bool check_files(FILE input_textfile, FILE output_countfile, FILE output_runtime){
+
+    bool opened_succesfully = true;
+
+    if (input_textfile == NULL){
+        fprintf(stderr, "Can't open input textfile");
+        opened_succesfully = false;
+    } 
+
+    if (output_countfile == NULL){
+        fprintf(stderr, "Can't open output countfile");
+        opened_succesfully = false;
+    }
+
+    if (output_runtime == NULL){
+        fprintf(stderr, "Can't open output runtime file");
+        opened_succesfully = false;
+    }
+
+    return opened_succesfully;
+
+}
